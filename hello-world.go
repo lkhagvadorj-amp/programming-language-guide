4d3085f8875400a7ceb0e2cdc0c16cef6c5b9707
lkhagvadorj-amp <lkhagvadorj.amp@gmail.com>
2021-11-08 10:36:08 +0800

    Basci Go language Lessons

---
+++ hello-world.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	// 「Hello World」となるように文字列を連結して出力してください
+	println("Hello " + "World")
+}
---
+++ variable.go
@@ -0,0 +1,25 @@
+package main
+
+func main() {
+	// int型変数 n に初期値として100を入れる
+	n := 100
+	// nから10を引いてnに代入しましょう
+	n -= 10
+
+	println(n)
+
+	// int型変数 m に初期値として10を入れる
+	m := 10
+	// mに1を足してmに代入しましょう
+	m++
+
+	println(m)
+
+	// string型変数 message に初期値として10を入れる
+	message := "Hello World"
+	// mに1を足してmに代入しましょう
+	message += " Go"
+
+	println(message)
+
+}
